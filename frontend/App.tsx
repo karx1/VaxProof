28a19354b0db8170039d10f62c614661c5f52c79
Yash <nerdstep710@gmail.com>
2021-04-09 10:23:35 -0500

    Create drawer navigator

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,13 +1,38 @@
+import { createDrawerNavigator, DrawerScreenProps } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import DrawerStackParamList from "./types";
+
+function HomeScreen({ navigation }: DrawerScreenProps<DrawerStackParamList, "Home">) {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome home!</Text>
+      <Button onPress={() => navigation.navigate("Notifications")} title="Go to notifs" />
+    </View>
+  )
+}
+
+function NotificationsScreen({ navigation }: DrawerScreenProps<DrawerStackParamList, "Notifications">) {
+  return (
+    <View style={styles.container}>
+      <Text>No notifs at the moment</Text>
+      <Button onPress={() => navigation.navigate("Home")} title="Go back home" />
+    </View>
+  )
+}
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
