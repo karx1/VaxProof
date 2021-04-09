0f503c86adc7e7108548578176078f6d62db47ea
Yash <nerdstep710@gmail.com>
2021-04-09 11:08:19 -0500

    Create API View for fetching the CSRF token

---
+++ backend/backend/views.py
@@ -0,0 +1,10 @@
+from django.middleware.csrf import get_token
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+@api_view(["GET"])
+@permission_classes([AllowAny])
+def get_csrf_token(request):
+    token = get_token(request)
+    return Response(token)
