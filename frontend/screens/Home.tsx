22b7b32ecfbefb920e3b66ff9e86f1fd28f3ca58
Yash <nerdstep710@gmail.com>
2021-04-09 11:47:17 -0500

    Convert home page to function component

--- frontend/screens/Home.tsx
+++ frontend/screens/Home.tsx
@@ -1,5 +1,5 @@
 import { DrawerScreenProps } from "@react-navigation/drawer";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DrawerStackParamList from "../types";
 import { API_URL } from "../constants";
 import axios from "axios";
@@ -8,31 +8,21 @@ import styles from "../styles";
 
 type Props = DrawerScreenProps<DrawerStackParamList, "Home">;
 
-interface IState {
-    authed: boolean
-}
+function Home(props: Props) {
+    const [authed, setAuthed] = useState(false);
 
-class Home extends React.Component<Props, IState> {
-    state = {
-        authed: false
-    }
-
-    constructor(props: Props) {
-        super(props);
-
+    useEffect(() => {
         axios.get(`${API_URL}/api/authed`).then(resp => {
-            console.log(resp.data);
+            setAuthed(resp.data);
         })
-    }
+    })
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>TODO</Text>
-            </View>
-        )
-    }
+    return (
+        <View style={styles.container}>
+            <Text>Hello!</Text>
+            {authed ? <Text>You are authenticated.</Text> : <Text>You are not authenticated.</Text>}
+        </View>
+    )
 }
-
 
 export default Home;
