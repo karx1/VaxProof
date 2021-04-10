1135e6dae674c91d37d61f9635127abac6e716ec
Yash <nerdstep710@gmail.com>
2021-04-10 13:45:29 -0500

    Create basic view for registering

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
 # Create your views here.
+
+@api_view(["POST"])
+@permission_classes([AllowAny])
+def register(request):
+    print(request.POST)
+    return Response({"detail": "yes"})
