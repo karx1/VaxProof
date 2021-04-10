1f04d429c0ef95395f0d29f9111574601a8dc005
Yash <nerdstep710@gmail.com>
2021-04-10 16:19:21 -0500

    Create login filter

--- frontend/screens/Login.tsx
+++ frontend/screens/Login.tsx
@@ -1,7 +1,8 @@
 import { DrawerNavigationProp } from "@react-navigation/drawer";
 import React from "react";
-import { View } from "react-native";
-import { Text } from "react-native-paper";
+import { KeyboardAvoidingView, View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import { Text, TextInput, Button } from "react-native-paper";
 import styles from "../styles";
 import DrawerStackParamList from "../types";
 
@@ -12,13 +13,35 @@ type Props = {
     token: string;
 }
 
-class Login extends React.Component<Props> {
+interface IState {
+    username: string;
+    password: string;
+}
+
+class Login extends React.Component<Props, IState> {
+    state = {
+        username: "",
+        password: ""
+    }
+
+    onChangeText = (name: string, text: string) => {
+        //@ts-ignore
+        this.setState({ [name]: text });
+    }
+    
     render() {
         return (
-            <View style={styles.container}>
-                <Text>Coming soon!</Text>
-                <Text>{this.props.token}</Text>
-            </View>
+            <ScrollView>
+                <KeyboardAvoidingView style={styles.container}>
+                    <Text style={styles.header}>Log In</Text>
+                    <View style={styles.input}>
+                        <TextInput label="Username" onChangeText={text => this.onChangeText("username", text)} mode="outlined" autoCapitalize="none" autoCompleteType="username" />
+                        <TextInput label="Password" onChangeText={text => this.onChangeText("username", text)} mode="outlined" autoCapitalize="none" autoCompleteType="password" secureTextEntry={true} />
+
+                        <Button mode="contained" icon="login-variant" style={styles.submit}>Log In</Button>
+                    </View>
+                </KeyboardAvoidingView>
+            </ScrollView>
         )
     }
 }
