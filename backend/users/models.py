2ce63d86072738e0efb2ce38805b580bfa6d2348
Yash <nerdstep710@gmail.com>
2021-04-10 13:16:17 -0500

    Create profile model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
