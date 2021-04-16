0ce1ca4be92f04b88666bf2123296eec0540fbc9
Yash Karandikar <nerdstep710@gmail.com>
2021-04-16 14:22:08 -0500

    Handle date and create object

--- backend/doses/views.py
+++ backend/doses/views.py
@@ -4,6 +4,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .serializers import DoseSerializer
 from pprint import pprint
+from dateutil.parser import parse
+from .models import Dose
 
 # Create your views here.
 
@@ -13,9 +15,17 @@ def new_dose(request):
     post = {k:v for k, v, in request.POST.items()}
     
     post["user"] = request.user.id
+    post["date"] = parse(post["date"]).date()
 
     serializer = DoseSerializer(data=post)
-    if not serializer.is_valid():
-        pprint(serializer.errors)
+    if serializer.is_valid():
+        data = serializer.validated_data
+        pprint(data)
 
-    return Response({"detail": "yes"})
+        Dose.objects.create(**data)
+
+        return Response({"detail": "Dose created successfully", "status": 201})
+    else:
+        resp = serializer.errors
+        resp["status"] = 400
+        return Response(resp)
