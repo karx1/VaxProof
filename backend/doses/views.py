f6a6e36912a11f6be52ea77311d518e2fd5afdb1
Yash Karandikar <nerdstep710@gmail.com>
2021-04-16 13:45:42 -0500

    Create validation check in the backend

--- backend/doses/views.py
+++ backend/doses/views.py
@@ -2,11 +2,20 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from .serializers import DoseSerializer
+from pprint import pprint
 
 # Create your views here.
 
 @api_view(["GET", "POST"])
 @permission_classes([IsAuthenticated])
 def new_dose(request):
-    print(request.POST)
+    post = {k:v for k, v, in request.POST.items()}
+    
+    post["user"] = request.user.id
+
+    serializer = DoseSerializer(data=post)
+    if not serializer.is_valid():
+        pprint(serializer.errors)
+
     return Response({"detail": "yes"})
